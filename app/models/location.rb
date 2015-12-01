80faf192c4bfac71df9d53b22ce4f3976e97c12d
armaanv <arm0206@gmail.com>
2015-11-30 21:14:58 -0800

    writeup

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,2 +1,6 @@
 class Location < ActiveRecord::Base
+  validates :name, uniqueness: true
+	validates :name, presence: true
+  has_many: User
+  has_many: poi
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ActiveRecord::Base
+  belongs_to: location
+  validates :phone, uniqueness: true
+  validates :phone, presence: true
+  validates :name, presence: true
+  validates :age, presence: true
+  validates :sex, presence: true
 end
