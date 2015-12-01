8b849cf537f4243f2c5771ba95533eb7f1c87308
David Liu <daveliu@berkeley.edu>
2015-11-30 19:37:18 -0800

    cleaned files

--- proj2/app/models/pokemon.rb
+++
@@ -1,5 +0,0 @@
-class Pokemon < ActiveRecord::Base
-	belongs_to :trainer
-	validates :name, uniqueness: true
-	validates :name, presence: true
-end
--- proj2/db/migrate/20151030194934_remove_health_from_pokemons.rb
+++
@@ -1,4 +0,0 @@
-class RemoveHealthFromPokemons < ActiveRecord::Migration
-  def change
-  end
-end
