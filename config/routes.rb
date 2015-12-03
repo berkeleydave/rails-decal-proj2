4c5b3d8b494c4eed6d5ed8fae03fb97d43179299
armaanv <arm0206@gmail.com>
2015-12-03 00:09:55 -0800

    profile page

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,15 @@ Rails.application.routes.draw do
   get 'pois/new'
 
   root to: 'home#index'
-  
-  
-  devise_for :users
-  resources :users
 
-  resources :users, only:[:new, :create, :destroy]
+
+  devise_for :users
+
+  resources :users
   resources :groups
 
   post "groups", to: "groups#create"
+  get "users/:id", to: "users#show"
 
   # patch "pokemons/:id/capture", to: "pokemons#capture", as: :capture
   # patch "pokemons/:id/damage", to: "pokemons#damage", as: :damage
