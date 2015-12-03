4f68a4a2c727d9b400fbd42de52c695c98ea6dd7
omkar <oraje@berkeley.edu>
2015-12-03 23:13:55 +0000

    geocoder user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151203225446) do
+ActiveRecord::Schema.define(version: 20151203230641) do
 
   create_table "groups", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -87,6 +87,8 @@ ActiveRecord::Schema.define(version: 20151203225446) do
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
     t.integer  "group_id"
+    t.float    "longitude"
+    t.float    "latitude"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
