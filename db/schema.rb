f411049d3f27a0e1bc908635b9ba8488b3f49854
omkar <oraje@berkeley.edu>
2015-12-03 05:07:43 +0000

    changed phone to string

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151202080017) do
+ActiveRecord::Schema.define(version: 20151203045453) do
 
   create_table "groups", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -70,7 +70,7 @@ ActiveRecord::Schema.define(version: 20151202080017) do
     t.string   "sex"
     t.string   "location"
     t.string   "interest"
-    t.integer  "phone"
+    t.string   "phone"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "email",                  default: "", null: false
